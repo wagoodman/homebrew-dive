82ec74cfc3f83d589f79356ce3e032a96811e8da
goreleaserbot <goreleaser@carlosbecker.com>
2018-11-06 21:59:31 -0500

    Brew formula update for dive version v0.2.0

--- dive.rb
+++ dive.rb
@@ -1,9 +1,9 @@
 class Dive < Formula
   desc ""
   homepage ""
-  url "https://github.com/wagoodman/dive/releases/download/v0.1.0/dive_0.1.0_darwin_amd64.tar.gz"
-  version "0.1.0"
-  sha256 "88f700786683e07723f88885e78a9a8c0dbb01e75584e2c8214beea5d8cdc649"
+  url "https://github.com/wagoodman/dive/releases/download/v0.2.0/dive_0.2.0_darwin_amd64.tar.gz"
+  version "0.2.0"
+  sha256 "67f748b6a94d8772e2111a36af9cc40e50f2abf686c1f53ead73567c1c876f1a"
 
   def install
     bin.install "dive"
