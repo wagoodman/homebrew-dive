4ccfe6f24f14776af5ba76ed3b37bc719b976907
David Yantis <404NetworkError@users.noreply.github.com>
2019-01-08 11:56:18 -0500

    Update dive to include desc and homepage
    In preparation of a stable release and merge into homebrew-core.

--- dive.rb
+++ dive.rb
@@ -1,6 +1,6 @@
 class Dive < Formula
-  desc ""
-  homepage ""
+  desc "A tool for exploring each layer in a docker image"
+  homepage "https://github.com/wagoodman/dive/"
   url "https://github.com/wagoodman/dive/releases/download/v0.6.0/dive_0.6.0_darwin_amd64.tar.gz"
   version "0.6.0"
   sha256 "6c671106cd01831fa100a20f795a8c2e89ba4f2f401b5aaf6d4f11de2858ee95"
