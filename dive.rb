d2cf73a30129f32e28c0ec16e345e0b4926b7d04
goreleaserbot <goreleaser@carlosbecker.com>
2019-04-06 13:30:31 -0400

    Brew formula update for dive version v0.7.2

--- dive.rb
+++ dive.rb
@@ -1,9 +1,9 @@
 class Dive < Formula
   desc "A tool for exploring each layer in a docker image"
   homepage "https://github.com/wagoodman/dive/"
-  url "https://github.com/wagoodman/dive/releases/download/v0.7.1/dive_0.7.1_darwin_amd64.tar.gz"
-  version "0.7.1"
-  sha256 "0b67093f90dc330e2d0f1d40f1890b52da30484223f39b2a4b859063c8044616"
+  url "https://github.com/wagoodman/dive/releases/download/v0.7.2/dive_0.7.2_darwin_amd64.tar.gz"
+  version "0.7.2"
+  sha256 "edb955ee7edc26f710ad8c6609c158ef5663753e1eadfde6b917644db11441b9"
 
   def install
     bin.install "dive"
